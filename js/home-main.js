000ee602cdf60b56e60fdf79c4620a7a0a534487
Eglė Juškevičienė <88egle@gmail.com>
2020-12-29 11:39:01 +0200

    file structure

---
+++ js/home-main.js
@@ -0,0 +1,32 @@
+/* header */
+/* hero */
+/* about */
+/* hobbie */
+/* achievements */
+/* services */
+/* job history */
+/* work expertise */
+/* portfolio */
+/* testimonials */
+/* blog */
+/* hire me banner */
+/* contact me */
+/* footer */
+
+
+// CODE EXECUTION
+
+/* header */
+/* hero */
+/* about */
+/* hobbie */
+/* achievements */
+/* services */
+/* job history */
+/* work expertise */
+/* portfolio */
+/* testimonials */
+/* blog */
+/* hire me banner */
+/* contact me */
+/* footer */
